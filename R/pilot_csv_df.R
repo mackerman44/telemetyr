48cd95c113bb629da991282d1f6825d2520769da
Mike Ackerman <mike.ackerman@biomark.com>
2020-04-08 16:46:39 -0600

    added pilot_csv_df with documentation

---
+++ R/pilot_csv_df.R
@@ -0,0 +1,9 @@
+#' @title Pilot Study Raw Receiver Data - .csv format
+#'
+#' @description The "raw" receiver download data, .csv format, from the Lemhi River radio telemetry pilot study
+#' @name pilot_csv_df
+#' @docType data
+#' @usage pilot_csv_df
+#' @format csv exports from Tracker software
+#' @keywords radio telemetry data pilot csv
+NULL
