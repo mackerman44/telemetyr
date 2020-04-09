6c1e25b16c733b4ace5fa16c81c2c4185c52a2ab
Mike Ackerman <mike.ackerman@biomark.com>
2020-04-08 18:15:04 -0600

    added some comments within read.csv.data function

--- R/read_csv_data.R
+++ R/read_csv_data.R
@@ -17,10 +17,12 @@ read.csv.data = function(path = ".") {
     filter(grepl(".csv$", nm))
 
   file_df %>%
+    # make a list of the file_name(s)
     select(file_name) %>%
     as.matrix() %>%
     as.character() %>%
     as.list() %>%
+    # read in each .csv file and assign col_names
     map_df(.f = function(x) {
       tmp = try(read_csv(paste(path, x[1], sep = "/"),
                          col_types = c("cccccc"),
@@ -31,19 +33,24 @@ read.csv.data = function(path = ".") {
                                        "end",
                                        "n")))
 
+      # if n of columns in tmp (a single csv file) is 0 or if class of tmp is error, return nothing
       if(ncol(tmp) == 0 | class(tmp)[1] == 'try-error') return(NULL)
 
       tmp = tmp %>%
+        # check and fix dates and times in start and end
         mutate_at(vars(start, end),
                   list(~ if_else(str_count(., '\\:') == 1,
                                  paste(., '00', sep = ':'),
                                  .))) %>%
+        # standardize date time information
         mutate_at(vars(start, end),
                   list(mdy_hms)) %>%
+        # make valid, tag_id, and n integers
         mutate_at(vars(valid, tag_id, n),
                   list(as.integer))
 
       return(tmp)
 
     })
+
 } # end read.csv.data()
