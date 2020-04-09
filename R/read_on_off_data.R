c728e4198a953615b3f811af4e4e5d143a972164
Mike Ackerman <mike.ackerman@biomark.com>
2020-04-09 12:09:50 -0600

    added read.on.off.data() function

---
+++ R/read_on_off_data.R
@@ -0,0 +1,37 @@
+#' @title Read in .csv Format Tracker Data
+#'
+#' @description Reads in informatio on receiver on/off times from the Tracker software
+#'
+#' @author Kevin See and Mike Ackerman
+#'
+#' @param path the directory containing the folders with observation data for each telemetry receiver for a study season
+#'
+#' @import dplyr readr
+#' @export
+#' @return a data frame containing receiver on/off information
+
+read.on.off.data = function(path = ".") {
+
+  # list all of the files with name ending with a single $
+  file_df = get.file.nms(path) %>%
+    filter(grepl("\\$", file_name),
+           !grepl("\\$\\$", file_name)) %>%
+    select(receiver, file_name)
+
+  on_off_df = NULL
+  for(i in 1:nrow(file_df)) {
+    on_off_df = on_off_df %>%
+      bind_rows(readr::read_table(paste(path, file_df$file_name[i], sep = "/"),
+                                  col_names = F) %>%
+                  mutate(receiver = file_df$receiver[i]))
+  }
+
+  on_off_df = on_off_df %>%
+    dplyr::select(receiver,
+                  on_off = X1,
+                  date = X2,
+                  time = X3)
+
+  return(on_off_df)
+
+} # end read.on.off.data()
--- analysis/pilot_study.R
+++ analysis/pilot_study.R
@@ -14,7 +14,6 @@
 #-------------------------
 # read in pilot study data from NAS
 #-------------------------
-
 # create df of file names
 pilot_path = "S:/telemetry/lemhi/fixed_site_downloads/2017_2018"
 file_df = get.file.nms(path = pilot_path)
@@ -26,6 +25,13 @@ save(pilot_csv_df, file = "data/raw/pilot_csv_df.rda")
 # this function reads in the "raw" text files
 pilot_txt_df = read.txt.data(path = pilot_path)
 save(pilot_txt_df, file = "data/raw/pilot_txt_df.rda")
+
+#-------------------------
+# deal with data that had previously been missing in the pilot study due to errors when resetting receiver
+# timers after downloading data
+#-------------------------
+miss_path = "S:/telemetry/lemhi/fixed_site_downloads/2017_2018_missing_A_data"
+
 
 #-------------------------
 # read in 2018-2019 season data from NAS
@@ -46,52 +52,6 @@ ssn_1819_txt_df = read.txt.data(path = ssn_1819_path)
 save(ssn_1819_txt_df, file = "data/raw/ssn_1819_txt_df.rda")
 
 #-------------------------
-# deal with data that had previously been missing in the pilot study due to errors when resetting receiver
-# timers after downloading data
+# read in pilot study on/off data from NAS
 #-------------------------
-miss_path = "S:/telemetry/lemhi/fixed_site_downloads/2017_2018_missing_A_data"
-
-########################################
-# deal with data that had been missing #
-########################################
-
-# Path to previously missing data
-missPath = '../Data/missingAdata/2017-2018'
-
-# missDataList = compress.txt.to.csv(missPath)
-
-# compress the txt files that contain the missing data to csv files. save as csv file if one doesn't exist
-# fileNms = str_split(names(missDataList), '\\/', simplify = T) %>%
-#   as.data.frame() %>%
-#   tbl_df() %>%
-#   rename(site = V1,
-#          txtFile = V2) %>%
-#   mutate(fileNm = if_else(as.integer(str_sub(txtFile, 1, 1)) >= 2,
-#                           paste0('17', txtFile),
-#                           paste0('18', txtFile)),
-#          fileNm = str_replace(fileNm, '.txt$', '.csv'))
-#
-# identical(nrow(fileNms), length(missDataList))
-#
-# for(i in 1:length(missDataList)) {
-#   missDataList[[i]] %>%
-#     mutate_at(vars(start, end),
-#               funs(as.character(paste0(month(.), '-', day(.), '-', year(.), ' ', hour(.), ':', minute(.), ':', second(.))))) %>%
-#     write_csv(paste(missPath, fileNms$site[i], fileNms$fileNm[i], sep = '/'),
-#               col_names = F)
-# }
-
-################
-# READ IN DATA #
-################
-
-# set path to receiver downloads
-rt_path = '../SiteDownloads/2017_2018'
-
-# get all the .csv files
-csv_df = read.csv.data(path = rt_path)
-
-# grab the data that had been missing
-miss_df = read.csv.data(path = missPath)
-
-
+on_off_df = read.on.off.data(path = pilot_path)
