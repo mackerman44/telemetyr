0e9a3f432317123c67367f26532905c835eb6e41
Kevin See <Kevin.See@merck.com>
2020-04-09 18:13:41 -0700

    added a few features to read.csv.data to make it more similar to read.txt.data

--- R/read_csv_data.R
+++ R/read_csv_data.R
@@ -4,53 +4,62 @@
 #'
 #' @author Kevin See
 #'
-#' @param path the directory containing the folders with observation data for each telemetry receiver for a study season
+#' @inheritParams get.file.nms
 #'
-#' @import dplyr purr readr stringr lubridate
+#' @import dplyr purrr readr stringr lubridate
 #' @export
 #' @return a data frame containing all of the .csv data
 
-read.csv.data = function(path = ".") {
+read.csv.data = function(path = ".",
+                         receiver_codes = NULL) {
 
   # df of the .csv files
-  file_df = get.file.nms(path) %>%
+  file_df = get.file.nms(path,
+                         receiver_codes) %>%
     filter(grepl(".csv$", nm))
 
-  file_df %>%
+  raw_csv = file_df %>%
+    # how many files for each receiver?
+    group_by(receiver) %>%
+    mutate(file_num = 1:n(),
+           n_files = n()) %>%
+    ungroup() %>%
     # make a list of the file_name(s)
-    select(file_name) %>%
-    as.matrix() %>%
-    as.character() %>%
-    as.list() %>%
-    # read in each .csv file and assign col_names
-    map_df(.f = function(x) {
-      tmp = try(read_csv(paste(path, x[1], sep = "/"),
-                         col_types = c("cccccc"),
-                         col_names = c("receiver",
-                                       "valid",
-                                       "tag_id",
-                                       "start",
-                                       "end",
-                                       "n")))
+    split(list(.$file_name)) %>%
+    map_df(.id = NULL,
+           .f = function(x) {
+             # read in each .csv file and assign col_names
+             cat(paste('Reading file', x$file_num, 'of', x$n_files, 'from receiver', x$receiver, '\n'))
 
-      # if n of columns in tmp (a single csv file) is 0 or if class of tmp is error, return nothing
-      if(ncol(tmp) == 0 | class(tmp)[1] == 'try-error') return(NULL)
+             tmp = try(read_csv(paste(path, x$file_name, sep = "/"),
+                                col_types = c("cccccc"),
+                                col_names = c("receiver",
+                                              "valid",
+                                              "tag_id",
+                                              "start",
+                                              "end",
+                                              "n")))
 
-      tmp = tmp %>%
-        # check and fix dates and times in start and end
-        mutate_at(vars(start, end),
-                  list(~ if_else(str_count(., '\\:') == 1,
-                                 paste(., '00', sep = ':'),
-                                 .))) %>%
-        # standardize date time information
-        mutate_at(vars(start, end),
-                  list(mdy_hms)) %>%
-        # make valid, tag_id, and n integers
-        mutate_at(vars(valid, tag_id, n),
-                  list(as.integer))
+             # if n of columns in tmp (a single csv file) is 0 or if class of tmp is error, return nothing
+             if(ncol(tmp) == 0 | class(tmp)[1] == 'try-error') return(NULL)
 
-      return(tmp)
+             tmp = tmp %>%
+               # check and fix dates and times in start and end
+               mutate_at(vars(start, end),
+                         list(~ if_else(str_count(., '\\:') == 1,
+                                        paste(., '00', sep = ':'),
+                                        .))) %>%
+               # standardize date time information
+               mutate_at(vars(start, end),
+                         list(lubridate::mdy_hms)) %>%
+               # make valid, tag_id, and n integers
+               mutate_at(vars(valid, tag_id, n),
+                         list(as.integer))
 
-    })
+             return(tmp)
+
+           })
+
+  return(raw_csv)
 
 } # end read.csv.data()
