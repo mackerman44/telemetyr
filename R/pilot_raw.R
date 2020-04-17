38250bef4a4d2ccf54e3c3215970766385d3044f
Kevin See <Kevin.See@merck.com>
2020-04-17 15:10:27 -0700

    deleted documentation about some datasets. Holding off until we know exactly what data should be included in this package.

--- R/pilot_raw.R
+++
@@ -1,9 +0,0 @@
-#' @title Pilot Study Raw Receiver Data - .txt format
-#'
-#' @description The "raw" receiver download data, .txt format, from the Lemhi River radio telemetry pilot study
-#' @name pilot_raw
-#' @docType data
-#' @usage pilot_raw
-#' @format txt exports from Tracker software
-#' @keywords radio telemetry data pilot txt
-NULL
