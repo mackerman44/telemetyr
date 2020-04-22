71685fefd257b951e7cc832510a240be37a4accb
Mike Ackerman <mike.ackerman@biomark.com>
2020-04-22 13:39:25 -0600

    updated parse_code_ending() and parse_tag_list() to use an argument data_to_parse instead of summ_data to differentiated from compress_df

--- R/parse_code_ending.R
+++ R/parse_code_ending.R
@@ -1,24 +1,28 @@
-#' @title Parse tags by tag code ending
+#' @title Parse Data Using a Code "Ending"
 #'
-#' @description Filter the summarised data to extract observations related to timer tags
+#' @description Filter a data frame to extract observations for tag codes ending in \code{code_ending};
+#' useful for extracting noise, timer, etc information.
 #'
 #' @author Kevin See and Mike Ackerman
 #'
-#' @param summ_data data.frame containing summarised data, output from \code{summarise_txt_data()}
-#' @param code_ending the ending of the tag code to filter for. Some examples include: "575$" for timer tags, "995$" for noise tags or "525$" for sentinel tags
+#' @inheritParams parse_tag_list
+#'
+#' @param code_ending the ending of the tag code to filter for. Some examples include: "575$" for timer tags,
+#' "995$" for noise tags, or "525$" for sentinel tags
 #'
 #' @import dplyr
 #' @export
-#' @return a data.frame containing a only records with the desired code ending
+#' @return a data frame containing only records for the desired \code{code_ending}
 
-parse_code_ending = function(summ_data = NULL,
+parse_code_ending = function(data_to_parse = NULL,
                              code_ending = NULL) {
 
-  stopifnot(!is.null(summ_data),
+  stopifnot(!is.null(data_to_parse),
             !is.null(code_ending))
 
-  timer_df = summ_data %>%
+  timer_df = data_to_parse %>%
     filter(grepl(code_ending, tag_id))
 
   return(timer_df)
+
 }
--- R/parse_tag_list.R
+++ R/parse_tag_list.R
@@ -1,24 +1,25 @@
-#' @title Parse tag data
+#' @title Parse Data Using a Tag List
 #'
-#' @description Filter the summarised data to extract observations related to tags, defined in a tag list
+#' @description Filter a data frame to extract observations for a list tags defined by \code{tags}
 #'
 #' @author Kevin See and Mike Ackerman
 #'
-#' @param summ_data data.frame containing summarised data, output from \code{summarise_txt_data()}
-#' @param tags vector of tag IDs that the user would like to extract data for
+#' @param data_to_parse a data frame to be "parsed" or filtered containing a column \code{tag_id}
+#' @param tags a vector of tag IDs that the user would like to extract data for
 #'
 #' @import dplyr
 #' @export
-#' @return a data.frame containing a summary of the tag data for the listed tags
+#' @return a data frame containing only records with a \code{tag_id} \%in\% \code{tags}
 
-parse_tag_list = function(summ_data = NULL,
+parse_tag_list = function(data_to_parse = NULL,
                           tags = NULL) {
 
-  stopifnot(!is.null(summ_data),
+  stopifnot(!is.null(data_to_parse),
             !is.null(tags))
 
-  tag_df = summ_data %>%
+  tag_df = data_to_parse %>%
     filter(tag_id %in% tags)
 
   return(tag_df)
+
 }
