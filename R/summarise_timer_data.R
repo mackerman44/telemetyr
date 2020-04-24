556651e8a20a2ec5cb1aab6c04333d49c98dc190
Mike Ackerman <mike.ackerman@biomark.com>
2020-04-24 07:50:19 -0600

    fixed bugs in summarise_timer_data()

--- R/summarise_timer_data.R
+++ R/summarise_timer_data.R
@@ -25,7 +25,7 @@ summarise_timer_data = function(compress_df = NULL,
     parse_code_ending(code_ending = "575$")
 
   # range of time among timer tags in timer_df
-  hr_range = lubridate::floor_date(range(timer_df$start), unit = "hours")
+  hr_range = lubridate::floor_date(range(timer_df$start, na.rm = T), unit = "hours")
   # if the season_start or season_end parameters are provided, use them to overwrite values in hr_range
   if(!is.null(season_start)) hr_range[1] <- season_start
   if(!is.null(season_end))   hr_range[2] <- season_end
@@ -47,33 +47,33 @@ summarise_timer_data = function(compress_df = NULL,
   cat("Summarizing hours that receivers are operational.\n")
 
   # data frame summarizing timer_df
-  tmp = timer_df %>%
-    filter(receiver %in% receiver_nms) %>%
-    mutate(hr = lubridate::floor_date(start,
-                                      unit = "hours")) %>%
-    filter(hr >= hr_range[1] & hr <= hr_range[2]) %>%
-    # include all hours in the hr_range. This adds a record to timer_data for each instance that no timer
-    # tag data exists for a receiver i.e. adds a record for each hour that a receiver was not operational
-    dplyr::full_join(expand.grid(list(receiver = receiver_nms,
-                                      hr = hr_range[1] + lubridate::dhours(seq(0, n_hrs)))) %>%
-                       tbl_df()) %>%
-    # when did each site first come online?
-    dplyr::left_join(timer_df %>%
-                       group_by(receiver) %>%
-                       summarise(receiver_start_hr = min(lubridate::floor_date(start,
-                                                                               unit = "hours"),
-                                                         na.rm = T)) %>%
-                       ungroup()) %>%
-    mutate(operational = ifelse(!is.na(n), 1, 0)) %>%
-    arrange(receiver, hr) %>%
-    # for each hour, is the receiver opeational?
-    group_by(receiver, receiver_start_hr, hr) %>%
-    summarise_at(vars(operational),
-                 funs(max)) %>%
-    mutate(operational = ifelse(operational == 1, T, F)) %>%
-    ungroup() %>%
-    mutate(receiver = factor(receiver,
-                             levels = receiver_nms))
+  tmp = suppressWarnings(timer_df %>%
+                         filter(receiver %in% receiver_nms) %>%
+                         mutate(hr = lubridate::floor_date(start,
+                                                           unit = "hours")) %>%
+                         filter(hr >= hr_range[1] & hr <= hr_range[2]) %>%
+                         # include all hours in the hr_range. This adds a record to timer_data for each instance that no timer
+                         # tag data exists for a receiver i.e. adds a record for each hour that a receiver was not operational
+                         dplyr::full_join(expand.grid(list(receiver = receiver_nms,
+                                                           hr = hr_range[1] + lubridate::dhours(seq(0, n_hrs)))) %>%
+                                            tbl_df()) %>%
+                         # when did each site first come online?
+                         dplyr::left_join(timer_df %>%
+                                            group_by(receiver) %>%
+                                            summarise(receiver_start_hr = min(lubridate::floor_date(start,
+                                                                                                    unit = "hours"),
+                                                                              na.rm = T)) %>%
+                                            ungroup()) %>%
+                         mutate(operational = ifelse(!is.na(n), 1, 0)) %>%
+                         arrange(receiver, hr) %>%
+                         # for each hour, is the receiver opeational?
+                         group_by(receiver, receiver_start_hr, hr) %>%
+                         summarise_at(vars(operational),
+                                      funs(max)) %>%
+                         mutate(operational = ifelse(operational == 1, T, F)) %>%
+                         ungroup() %>%
+                         mutate(receiver = factor(receiver,
+                                                  levels = receiver_nms)))
 
   cat("Plotting operation times.\n")
 
@@ -96,19 +96,19 @@ summarise_timer_data = function(compress_df = NULL,
 
   # calculate the proportion of time from season_start (min_hr) to season_end (max_hr) that each receiver
   # was operational
-  tmp_p_op = tmp %>%
-    mutate(site = substr(receiver, 1, 2)) %>%
-    left_join(tmp %>%
-                dplyr::filter(operational == T) %>%
-                dplyr::group_by(receiver) %>%
-                dplyr::summarise(receiver_end_hr = max(lubridate::floor_date(hr,
-                                                                             unit = "hours"),
-                                                       na.rm = T)) %>%
-                ungroup()) %>%
-    filter(hr >= receiver_start_hr & hr <= receiver_end_hr) %>%
-    group_by(receiver) %>%
-    summarise(p_operational = sum(operational == T) / length(operational)) %>%
-    ungroup()
+  tmp_p_op = suppressWarnings(tmp %>%
+                              mutate(site = substr(receiver, 1, 2)) %>%
+                              left_join(tmp %>%
+                                          dplyr::filter(operational == T) %>%
+                                          dplyr::group_by(receiver) %>%
+                                          dplyr::summarise(receiver_end_hr = max(lubridate::floor_date(hr,
+                                                                                                       unit = "hours"),
+                                                                                 na.rm = T)) %>%
+                                          ungroup()) %>%
+                              filter(hr >= receiver_start_hr & hr <= receiver_end_hr) %>%
+                              group_by(receiver) %>%
+                              summarise(p_operational = sum(operational == T) / length(operational)) %>%
+                              ungroup())
 
   tmp_list = list(operations_summ = tmp,
                   operations_plot = tmp_plot,
