c07f38c79262760e995ea42f7065c8b4ddfd927f
Mike Ackerman <mike.ackerman@biomark.com>
2020-05-06 05:39:13 -0600

    added "compressed" example dataset

--- R/data.R
+++ R/data.R
@@ -18,6 +18,24 @@
 #' @source Biomark NAS - data/telemetry/lemhi/fixed_site_downloads/2018_2019/
 "raw"
 
+#' Compressed Telemetry Data
+#'
+#' An example dataset containing compressed observations
+#' for 5 receivers and odd numbered frequencies, 2018/2019 season
+#'
+#' @format A data frame with 260437 rows and 7 variables:
+#' \describe{
+#'   \item{receiver}{receiver code}
+#'   \item{valid}{is observation valid, 1 = T, 0 = F}
+#'   \item{tag_id}{tag ID including the frequency and code}
+#'   \item{start}{the time of first observation in "\%Y-\%m-\%d \%H:\%M:\%S" format}
+#'   \item{end}{the time of last observation in "\%Y-\%m-\%d \%H:\%M:\%S" format}
+#'   \item{n}{number of observations}
+#'   \item{week}{the week in the study season}
+#' }
+#' @source Biomark NAS - data/telemetry/lemhi/fixed_site_downloads/2018_2019/
+"compressed"
+
 #' Volt & Temperature Data
 #'
 #' An example dataset containing voltage and temperature data recorded
