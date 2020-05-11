64339edb5ab39313e90df87fe0a2f87e93893024
Kevin See <Kevin.See@merck.com>
2020-05-11 15:09:26 -0700

    re-saved tag_releases data to include a "tag_id" column", and also to ensure that dates/times weren't messed up by reading in an Excel file.

--- data-raw/tag_releases.R
+++ data-raw/tag_releases.R
@@ -3,7 +3,15 @@
 library(telemetyr)
 library(readxl)
 
-tag_releases = read_excel("../data/prepped/tag_release/lemhi_winter_telemetry_tag_info.xlsx")
+tag_releases = read_excel("data/prepped/tag_release/lemhi_winter_telemetry_tag_info.xlsx") %>%
+  mutate(tag_id = str_extract(radio_tag_id, "[:digit:]*"),
+         tag_id = as.numeric(tag_id)) %>%
+  mutate_at(vars(activation_time, release_time),
+            list(as.numeric)) %>%
+  mutate_at(vars(activation_time, release_time),
+            list(janitor::excel_numeric_to_date),
+            include_time = T) %>%
+  select(season, matches('tag_id'), everything())
 
 write_csv(tag_releases, "data-raw/tag_releases.csv")
 usethis::use_data(tag_releases, overwrite = TRUE)
