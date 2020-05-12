c280ab15893f3ea956c89dbc3cc88e53f4234ccd
Mike Ackerman <mike.ackerman@biomark.com>
2020-05-12 10:28:21 -0600

    fixed n rows for compressed documentation to match compressed.rda

--- R/data.R
+++ R/data.R
@@ -23,7 +23,7 @@
 #' An example dataset containing compressed observations
 #' for 5 receivers and 1 frequency, 2018/2019 season
 #'
-#' @format A data frame with 66468 rows and 7 variables:
+#' @format A data frame with 66449 rows and 7 variables:
 #' \describe{
 #'   \item{receiver}{receiver code}
 #'   \item{valid}{is observation valid, 1 = T, 0 = F}
