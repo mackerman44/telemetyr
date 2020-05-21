74b58f4dd2191af0f4c0bc2aa8c4d20eb91620d5
Kevin See <Kevin.See@merck.com>
2020-05-21 13:02:42 -0700

    fixed one more issue that was causing functions to not put sites in user-defined order.

--- R/summarise_timer_data.R
+++ R/summarise_timer_data.R
@@ -50,7 +50,8 @@ summarise_timer_data = function(compress_df = NULL,
   # if user provides a list of receiver codes
   if(!is.null(receiver_codes)) {
     receiver_nms = factor(receiver_nms[receiver_nms %in% receiver_codes],
-                          levels = receiver_codes)
+                          levels = receiver_codes) %>%
+      sort()
   }
 
   cat("Summarizing hours that receivers are operational.\n")
