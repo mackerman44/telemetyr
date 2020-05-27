41e42fb7ec61aa764bea7285b2d64f98def3ef64
Kevin See <Kevin.See@merck.com>
2020-05-27 09:13:46 -0700

    renamed prep_hab_avail to prep_hab_data

--- analysis/prep_hab_data.R
+++ analysis/prep_hab_data.R
@@ -20,7 +20,7 @@ library(tidyverse)
 library(sf)
 library(janitor)
 library(lubridate)
-#library(telemetyr)
+library(raster)
 
 theme_set(theme_bw())
 
@@ -34,6 +34,9 @@ if(.Platform$OS.type == 'unix') {
   nas_prefix = "~/../../Volumes/ABS"
 }
 
+##########################
+# AVAILABILITY DATA PREP #
+##########################
 #-------------------------
 # read in centerline x xs intersections
 #-------------------------
@@ -44,6 +47,7 @@ xs_center_pts = st_read(paste(nas_prefix,
   mutate(Category = fct_relevel(Category, "High", after = Inf))
 my_crs = st_crs(xs_center_pts)
 
+# quick summary by sinuosity Category
 xs_center_pts %>%
   st_drop_geometry() %>%
   as_tibble() %>%
@@ -55,12 +59,12 @@ xs_center_pts %>%
   mutate(perc = n_pts / sum(n_pts))
 
 #-------------------------
-# read in habitat availability data
+# read in habitat availability data that have been QA/QC'd
 #-------------------------
-hab_avail_path = paste(nas_prefix, "data/habitat/lemhi_telemetry/availability/prepped/", sep = '/')
+hab_avail_path = paste(nas_prefix, "data/habitat/lemhi_telemetry/availability/", sep = '/')
 
 # "0" points on transects, meant to match the xs_center_pts
-avail_pts_org = read_csv(paste0(hab_avail_path, "Habitat_Avail_1.csv")) %>%
+avail_pts_org = read_csv(paste0(hab_avail_path, "prepped/Habitat_Avail_1.csv")) %>%
   clean_names(case = "snake") %>%
   mutate_at(vars(creation_date, edit_date),
             list(mdy_hm))
@@ -83,7 +87,7 @@ avail_pts_sf = avail_pts %>%
            crs = "+proj=longlat +datum=WGS84") %>%
   st_transform(my_crs)
 
-# plot them to make sure we're good
+# plot the availability transect points along with all cross-section points
 xs_avail_p = xs_center_pts %>%
   ggplot() +
   geom_sf(aes(color = Category,
@@ -92,7 +96,8 @@ xs_avail_p = xs_center_pts %>%
   geom_sf(data = avail_pts_sf,
           size = 0.25) +
   labs(fill = "Sinuosity\nCategory",
-       color = "Sinuosity\nCategory")
+       color = "Sinuosity\nCategory",
+       title = "Availability Transect Points")
 xs_avail_p
 
 # for each availability pt, what is nearest xs?
@@ -117,7 +122,7 @@ xs_2_avail %>%
 #-------------------------
 # read in habitat availability data
 #-------------------------
-xs_pts = read_csv(paste0(hab_avail_path, "Point_Number_2.csv")) %>%
+xs_pts = read_csv(paste0(hab_avail_path, "prepped/Point_Number_2.csv")) %>%
   clean_names(case = 'snake') %>%
   select(-object_id,
          -creation_date,
@@ -125,7 +130,7 @@ xs_pts = read_csv(paste0(hab_avail_path, "Point_Number_2.csv")) %>%
          -edit_date,
          -editor)
 
-sc_pts = read_csv(paste0(hab_avail_path, "Side_Channel_Info_3.csv")) %>%
+sc_pts = read_csv(paste0(hab_avail_path, "prepped/Side_Channel_Info_3.csv")) %>%
   clean_names(case = 'snake') %>%
   select(-object_id,
          -creation_date,
@@ -185,7 +190,7 @@ xs_avail %>%
 save(xs_avail,
      avail_pts_sf,
      file = paste(nas_prefix,
-                  "data/habitat/lemhi_telemetry/prepped/habitat_available.rda",
+                  "data/habitat/lemhi_telemetry/availability/prepped/habitat_available.rda",
                   sep = "/"))
 
 avail_pts_sf %>%
@@ -258,12 +263,74 @@ xs_avail %>%
   labs(x = 'Sinuousity Class',
        y = 'Percentage')
 
+#-------------------------
+# Extract depth and velocity values from 2d numerical model results
+# by sinuosity category
+#-------------------------
+# depth & velocity rasters from 2d numerical model, lemhi winter scenario
+d_raster <- raster(paste0(nas_prefix, "data/habitat/HSI/MRA_d_v_tifs/lemhi/d_jan_v2.tif"))
+v_raster <- raster(paste0(nas_prefix, "data/habitat/HSI/MRA_d_v_tifs/lemhi/v_jan_v2.tif"))
 
-#-------------------------
-# read in habitat use data
-#-------------------------
+# read in the polygons that designate the Low/Med/High sinuosity reaches
+sin_polygons = st_read(paste0(hab_avail_path, "raw/Sin_polys.shp"))
+
+sin_polygons %>%
+  ggplot() +
+  geom_sf(aes(fill = sin_class,
+              color = sin_class)) +
+  labs(fill = "Sinuosity\nClass",
+       color = "Sinuosity\nClass") +
+  theme_bw()
+
+# create one sf object for each sinuosity class
+sin_names = unique(sin_polygons$sin_class)
+for(sin in sin_names) {
+  assign(as.character(sin), filter(sin_polygons, sin_class == sin))
+}
+
+# extract depth raster values according to sinuosity polygons
+# for(sin in sin_names) {
+#   sf_tmp = raster::extract(d_raster,
+#                            get(as.character(sin)),
+#                            fun = NULL,
+#                            df = TRUE,
+#                            na.rm = TRUE) %>%
+#     drop_na() %>%
+#     mutate(sin_class = as.character(sin))
+#   assign(paste0("d_extract_", sin), sf_tmp)
+#   rm(sf_tmp)
+# }
+#
+# depth_extract = bind_rows(lapply(ls(pattern = "^d_extract"), function(x) get(x))) %>%
+#   rename(value = d_jan_v2)
+# depth_extract$metric = "depth"
+
+# extract velocity raster values according to sinuosity polygons
+# for(sin in sin_names) {
+#   sf_tmp = raster::extract(v_raster,
+#                            get(as.character(sin)),
+#                            fun = NULL,
+#                            df = TRUE,
+#                            na.rm = TRUE) %>%
+#     drop_na() %>%
+#     mutate(sin_class = as.character(sin))
+#   assign(paste0("v_extract_", sin), sf_tmp)
+#   rm(sf_tmp)
+# }
+
+velocity_extract = bind_rows(lapply(ls(pattern = "^v_extract"), function(x) get(x))) %>%
+  rename(value = v_jan_v2)
+velocity_extract$metric = "velocity"
+
+d_v_avail_sin = bind_rows(depth_extract, velocity_extract)
+save(d_v_avail_sin,
+     file = paste0(hab_avail_path, "prepped/d_v_avail_sin.rda"))
+
+#########################
+# HABITAT USE DATA PREP #
+#########################
 hab_use_path = paste(nas_prefix, "data/habitat/lemhi_telemetry/use/", sep = '/')
-use_pts = read_csv(paste0(hab_use_path, "Fish_Info_Master_NPedits.csv")) %>%
+use_pts = read_csv(paste0(hab_use_path, "raw/Fish_Info_Master_NPedits.csv")) %>%
   clean_names(case = "snake")
 
 # convert to sf object
@@ -285,7 +352,8 @@ xs_avail_p = xs_center_pts %>%
           color = "white",
           size = 0.25) +
   labs(fill = "Sinuosity\nCategory",
-       color = "Sinuosity\nCategory")
+       color = "Sinuosity\nCategory",
+       title = "Use and Availability Pts")
 xs_avail_p
 
 # for each use pt, what is nearest xs?
@@ -313,11 +381,11 @@ xs_use = xs_2_use %>%
   rename(bank_type_condition_closest = bank_type_condition,
          dominant_cover_type_1_5m_radius = dominant_cover_1_5m_radius,
          distance_to_cover_round_to_nearest_0_1m = distance_to_cover_m) %>%
-  select(global_id,
-         date:habitat_selected,
-         one_of(names(xs_avail)),
-         depth_m:notes,
-         x, y) %>%
+  dplyr::select(global_id,
+                date:habitat_selected,
+                one_of(names(xs_avail)),
+                depth_m:notes,
+                x, y) %>%
   mutate(channel_unit_type = recode(channel_unit_type,
                                     "Rapid Plus" = "Rapid+",
                                     "Off Channel Area" = 'OCA',
@@ -349,18 +417,16 @@ xs_use = xs_2_use %>%
                                                   levels = c(levels(xs_avail$dominant_cover_type_1_5m_radius), "Unknown"))) %>%
   arrange(radio_frequency, date)
 
-
 # save for later
 save(xs_use,
      use_pts_sf,
-     file = paste(nas_prefix,
-                  "data/habitat/lemhi_telemetry/prepped/habitat_use.rda",
-                  sep = "/"))
+     file = paste0(hab_use_path,
+                  "prepped/habitat_use.rda"))
 
 # make spatial again
 xs_use_sf = xs_use %>%
   left_join(use_pts_sf %>%
-              select(global_id)) %>%
+              dplyr::select(global_id)) %>%
   st_as_sf()
 
 xs_use_sf %>%
@@ -381,8 +447,6 @@ xs_use %>%
   adorn_totals(where = 'col') %>%
   adorn_percentages() %>%
   adorn_pct_formatting()
-
-
 
 xs_use %>%
   filter(tag_status != 'Mortality') %>%
@@ -475,7 +539,6 @@ xs_all %>%
   labs(x = 'Data Set',
        y = 'Percentage')
 
-
 xs_all %>%
   # filter(source == 'Avail.' |  tag_status != 'Mortality') %>%
   filter(source == 'Avail.' |  habitat_selected == 'Selected') %>%
@@ -500,3 +563,5 @@ xs_center_pts %>%
   st_drop_geometry() %>%
   tabyl(Category) %>%
   adorn_pct_formatting()
+
+
